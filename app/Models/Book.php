715ef61ce8b0316139bababe24db194d77af0e14
tanaka katsuya <gu0520kt@gmail.com>
2021-05-15 08:37:16 +0900

    BookとTagのモデルの修正

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+    ];
+
+    public function tags()
+    {
+        return $this->belongsToMany('App\Models\Tag')->withTimestamps();
+    }
 }
